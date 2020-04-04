c3adb0453f370428b227841d6e6d9b9e2c335fa5
邱娜 <nala@Luna.local>
2020-04-05 01:08:48 +0800

    init ToolBar

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="tool-bar">
+    <!-- tab 有数据源，根据数据源驱动视图 -->
+    <div class="tool-bar-item"></div>
+  </div>
+
+</template>
+
+<script>
+/**
+ * ToolBar 功能
+ * 1、位于页面最底部
+ * 2、点击 toobar 按钮时，页面发生对应的切换
+ * 3、按钮分为 默认 和 选中 两个状态
+ */
+export default {
+data: () => {
+  return {
+    toolBarData: []
+  }
+}    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
